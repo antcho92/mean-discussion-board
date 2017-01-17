3f05bf7619e10855922693abdc9940dc4e6bc5a5
antcho92 <antcho92@gmail.com>
2017-01-17 08:47:03 -0800

    added mongoose connection logs

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,10 +1,30 @@
 var mongoose = require('mongoose'),
     path = require('path'),
     fs = require('fs'),
-    modelsPath = path.join(__dirname, './../models');
+    modelsPath = path.join(__dirname, './../models'),
+    dbURI = 'mongodb://localhost/discussion';
 
-mongoose.connect('mongodb://localhost/discussion');
+mongoose.connect(dbURI);
 mongoose.Promise = global.Promise;
+
+mongoose.connection.on( 'connected', function () {
+  console.log( `Mongoose default connection open to ${ dbURI }` );
+});
+
+mongoose.connection.on( 'error', function ( err ) {
+  console.error( `Mongoose default connection error: ${ err }` );
+});
+/* When the connection is disconnected */
+mongoose.connection.on( 'disconnected', function () {
+  console.log( 'Mongoose default connection disconnected' );
+});
+/* If the Node process ends, close the Mongoose connection */
+process.on( 'SIGINT', function() {
+  mongoose.connection.close( function () {
+    console.log( 'Mongoose default connection disconnected through app termination' );
+    process.exit( 0 );
+  });
+});
 
 fs.readdirSync(modelsPath).forEach(function(file) {
   if (file.indexOf('.js') >= 0) {
